97105a1e9d9f1a615ee8c34dfffd5c8dfa14fc7c
laptop admin <masa.namba101@gmail.com>
2025-11-06 01:02:42 -0800

    idk

---
+++ src/main/java/org/plugin/twopeeplugin/Core/mostPopularTracker.java
@@ -0,0 +1,53 @@
+package org.plugin.twopeeplugin.Core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.plugin.twopeeplugin.Utils.courseYamlConfig;
+
+public class mostPopularTracker implements Listener {
+	private HashMap<String,Integer> mapping;
+	private String maxKey = "Raspberry511";
+
+	public mostPopularTracker()  {
+		mapping = new HashMap<>();
+		countPlayers();
+	}
+
+	// @EventHandler
+	// private void onPlayerChangeWorld(PlayerChangedWorldEvent event) {
+	// 	countPlayers();
+	// }
+	//
+	// public String getMostPopularMap() {
+	// 	int max = 0;
+	// 	for (Map.Entry<String, Integer> entry : mapping.entrySet()) {
+	// 		if (entry.getValue() > max && !entry.getKey().equals("lobby") && isVerified(entry.getKey())) {
+	// 			max = entry.getValue();
+	// 			maxKey = entry.getKey();
+	// 		}
+	// 	}
+	// 	return maxKey;
+	// }
+
+	private boolean isVerified(String worldName) {
+		return courseYamlConfig.getConfig().getBoolean("course." + worldName + ".verified");
+	}
+
+
+	private void countPlayers() {
+		mapping.clear();
+		for (Player player:Bukkit.getOnlinePlayers()) {
+			if (player.isOnline()) {
+				String worldName = player.getWorld().getName();
+				mapping.put(worldName, mapping.getOrDefault(worldName, 0) + 1);
+			}
+		}
+	}
+
+}
---
+++ update.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+scp -rf -i 2pProdServer_key.pem ~/project/devserver/ azureuser@20.172.64.185:~
