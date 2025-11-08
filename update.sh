618aeb39478aeb5e330b65eccee23a0ec5b4dec0
laptop admin <masa.namba101@gmail.com>
2025-11-07 23:45:17 -0800

    fix fix

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-scp -rf -i 2pProdServer_key.pem ~/project/devserver/plugins/two.pee.plugin-1.0-SNAPSHOT.jar azureuser@20.172.64.185:~/server/plugins/two.pee.plugin-1.0-SNAPSHOT.jar
+scp ~/project/devserver/plugins/two.pee.plugin-1.0-SNAPSHOT.jar azureuser@20.172.64.185:~/server/plugins/two.pee.plugin-1.0-SNAPSHOT.jar
+
